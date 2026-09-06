a38fe60d231bf3830cea0f04e53de6b011eb7d65
Andressa Lessa <s.andressalessa@gmail.com>
2026-09-06 20:25:42 -0300

    feat: adding subscribeToQueue

--- checkout-service/src/events/rabbitmq/rabbitmq.service.ts
+++ checkout-service/src/events/rabbitmq/rabbitmq.service.ts
@@ -123,4 +123,55 @@ export class RabbitmqService implements OnModuleInit, OnModuleDestroy {
       this.logger.error(`❌ Error publishing message to RabbitMQ:`, error);
     }
   }
+
+  async subscribeToQueue(
+    queueName: string,
+    exchange: string,
+    routingKey: string,
+    callback: (message: unknown) => Promise<void>,
+  ): Promise<void> {
+    try {
+      if (!this.channel) {
+        this.logger.warn('⚠️ RabbitMQ channel is not available.');
+        return;
+      }
+
+      await this.channel.assertExchange(exchange, 'topic', { durable: true });
+      const queue = await this.channel.assertQueue(queueName, {
+        durable: true,
+        arguments: {
+          'x-message-ttl': 86400000,
+          'x-max-length': 10000,
+        },
+      });
+
+      await this.channel.bindQueue(queue.queue, exchange, routingKey);
+      await this.channel.prefetch(1);
+      await this.channel.consume(queue.queue, async (msg) => {
+        if (msg) {
+          try {
+            const message: unknown = JSON.stringify(msg.content.toString());
+            this.logger.log(`📨 Message received from queue: ${queueName}`);
+            this.logger.debug(`Message content: ${JSON.stringify(message)}`);
+            await callback(message);
+
+            this.channel.ack(msg); // removes msg from channel (accept message)
+
+            this.logger.log(
+              `✅ Message processed succesfully from queue: ${queueName}`,
+            );
+          } catch (error) {
+            this.logger.error(`❌ Error processing message:`, error);
+            this.channel.nack(msg, false, false); // rejects message (in prod send to DLQ)
+          }
+        }
+      });
+
+      this.logger.log(
+        `✅ Subscribed to queue: ${queueName} with routing key: ${routingKey}`,
+      );
+    } catch (error) {
+      this.logger.error(`❌ Error to subscribe to queue: ${queueName}`, error);
+    }
+  }
 }
