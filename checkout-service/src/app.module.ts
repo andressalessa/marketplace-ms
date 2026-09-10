5fba542c041aa96dfac3d1413f226a834a932030
Andressa Lessa <s.andressalessa@gmail.com>
2026-09-10 20:01:29 -0300

    feat: update checkout-service

--- checkout-service/src/app.module.ts
+++ checkout-service/src/app.module.ts
@@ -1,20 +1,35 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
+import { OrdersModule } from './orders/orders.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { HttpMetricsMiddleware } from './metrics/http-metrics.middleware';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // available for all modules
+      isGlobal: true,
     }),
     TypeOrmModule.forRoot(databaseConfig),
+    MetricsModule,
     EventsModule,
+    AuthModule,
+    CartModule,
+    OrdersModule,
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HttpMetricsMiddleware).forRoutes('*');
+  }
+}
