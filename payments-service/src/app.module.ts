3df25034dad11a70de3f702f43794a4507614b8e
Andressa Lessa <s.andressalessa@gmail.com>
2026-09-11 20:01:15 -0300

    fix: fixing payments module and install missing packages

--- payments-service/src/app.module.ts
+++ payments-service/src/app.module.ts
@@ -1,20 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
+import { MetricsModule } from './metrics/metrics.module';
+import { HttpMetricsMiddleware } from './metrics/http-metrics.middleware';
 import { EventsModule } from './events/events.module';
+import { PaymentsModule } from './payments/payments.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, // available for all modules
-    }),
     TypeOrmModule.forRoot(databaseConfig),
+    MetricsModule,
     EventsModule,
+    PaymentsModule,
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(HttpMetricsMiddleware).forRoutes('*');
+  }
+}
